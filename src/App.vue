8ad7a5daa202befc3073398573a557cf4b993b1b
Gia Huy Tran <tonytran0314@gmail.com>
2024-05-04 15:37:10 -0400

    menu section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
+  import Menu from './components/Menu.vue'
+  import HeroSection from './components/HeroSection.vue'
+  import About from './components/About.vue'
+  import Projects from './components/Projects.vue'
+  import Contact from './components/Contact.vue'
+  import Footer from './components/Footer.vue'
 </script>
 
 <template>
+  <Menu />
 </template>
 
 <style lang="scss" scoped>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,11 +3,70 @@
 </script>
 
 <template>
-    <div>
-        
+    <div id="menu">
+        <div id="logo">
+            <h3><span>&lt;</span>htran <span>/&gt;</span></h3>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">
+                        <h6>Home</h6>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h6>About Me</h6>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h6>Projects</h6>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <h6>Contact</h6>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+
+    @import '/src/assets/variables';
+
+    #menu {
+        width: 100%;
+        height: 5rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 1.5rem;
+
+        #logo {
+            span {
+                color: $darkLabel;
+            }
+        }
+
+        nav {
+
+            ul {
+                display: flex;
+                gap: 4rem;
+
+                li {
+                    list-style-type: none;
+
+                    a {
+                        text-decoration: none;
+                    }
+                }
+            }
+        }
+    }
     
 </style>
